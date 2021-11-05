07f6a0ede11f383fdb2e92c6d30c22c590caf0a6
Justin Tu <coppolavina@gmail.com>
2021-11-05 04:39:40 +0000

    revise breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,29 @@
 
+const { Agent } = require('http');
 const request = require('request');
 
 const args = process.argv.slice(2);
 
 //// breed name is entered via command line agurment
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error,body) => {
-  /// in case breed of page not found
-  if (error === null) {
-    return console.log('error: page not found');
-  } else if (error !== undefined) {/// in case request failed, example wrong domain
-      return console.log('request failed');
-  } else {
-      const fs = require('fs'); //this function to read data returned from thecatapi
-      fs.readFile(body, () => {
-    // console.log(`${body}`);
-    // console.log(typeof body)
-      const data = JSON.parse(body);
-      console.log(data[0]["description"]);
-      });
-    };
-});
+const fetchBreedDescription = function (x) {
+    request(`https://api.thecatapi.com/v1/breeds/search?q=${x}`, (error,response,body) => {
+    //const data = JSON.parse(body);
+    // console.log(data.length === 0)
+    //in case page not found
+      if (body === undefined){
+      return console.log('request failed');}
+    const data = JSON.parse(body)
+    if (data.length === 0) {/// in case request failed, example wrong domain
+        return console.log('page not found');
+    } else {
+        const fs = require('fs'); //this function to read data returned from thecatapi
+        fs.readFile(body, () => {
+      // console.log(`${body}`);
+      // console.log(typeof body)
+        console.log(data[0]["description"]);
+        });
+      };
+    });
+  }
 
-  
+  fetchBreedDescription(args[0])
